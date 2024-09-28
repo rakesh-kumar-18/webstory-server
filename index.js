1d82e87c6a5bd7336d66a4485a21a70f8c420dfa
Rakesh Kumar Sahoo <sahoorakeshkumar42@gmail.com>
2024-09-28 11:19:51 +0530

    remove cors

--- index.js
+++ index.js
@@ -11,7 +11,7 @@ const userRoutes = require("./routes/user");
 
 const app = express();
 
-app.use(cors({ origin: process.env.CORS_ORIGIN }));
+app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
